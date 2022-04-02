93846b44ef7a9e6aecd7074e7db56119ff11ed31
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-04-02 12:57:09 +0100

    some modifications to routes, pause for now, come back later, moving onto messaging feature

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,7 +5,8 @@ router.post("/post", postControllers.createNewPost);
 router.put("/post/:id", postControllers.updatePost);
 router.delete("/post/:id", postControllers.deletePost);
 router.get("/post/:id", postControllers.getPost);
+router.get("/posts/all", postControllers.getAllPosts);
 router.put("/post/:id/like", postControllers.likePost);
-router.get("/post/timeline/feed", postControllers.getTimeline);
+router.get("/post/timeline/:userId", postControllers.getTimeline);
 
 module.exports = router;
