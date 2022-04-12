ac8450c84c6d7a0db2a973117ba9ce3b42c5995d
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-04-12 09:32:06 +0100

    minor modifications

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 // initial library installation set up
 const express = require("express");
 const app = express();
-const appS = express();
 const mongoose = require("mongoose");
 const JobModel = require("./models/jobModels");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
-const http = require("http");
-const { Server } = require("socket.io");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -52,59 +49,5 @@ app.get("/", (_req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(
-    `Builders' Collective Server listening on http://localhost:${PORT}`
-  );
-});
-
-// messaging/My Network feature
-//--------------------------------------------------
-
-// establish connection to new instance "server"
-const server = http.createServer(appS);
-
-const ORIGIN = process.env.ORIGIN;
-
-// cors management safe guarding
-const io = new Server(server, {
-  cors: {
-    origin: ORIGIN,
-    methods: ["GET", "POST"],
-  },
-});
-
-// listening for event with "connection" id
-io.on("connection", (socket) => {
-  //welcomes current user
-  socket.emit("message", "Welcome to the Builders' Collective Chat Stream");
-
-  // broadcast to other users when a new user connects
-  socket.broadcast.emit("message", "A user has joined the chat");
-
-  // upon joining a room
-  socket.on("join_room", (data) => {
-    socket.join(data);
-  });
-
-  // Listen for sendMessage() function for message sent from chat room
-  socket.on("send_message", (messageData) => {
-    socket.to(messageData.room).emit("receive_message", messageData);
-  });
-
-  // lets all users know when a user disconnects
-  socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
-  });
-});
-
-const SOCKET_PORT = process.env.SOCKET_PORT || 4040;
-
-appS.get("/", (_req, res) => {
-  res.send("Welcome to the Builders' Collective Socket Server!");
-});
-
-server.listen(SOCKET_PORT, () => {
-  console.log(
-    `Builders' Collective Network Chat listening on http://localhost:${SOCKET_PORT}`
-  );
+  console.log(`Builders' Collective Server listening on Port: ${PORT}`);
 });
