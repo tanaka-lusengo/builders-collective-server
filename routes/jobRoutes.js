6d29b39d25377fd2ce3637ea2fa4174b3d003edd
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-05-18 22:13:49 +0100

    update jobs route to have less calls to speed up initial site load time

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -3,14 +3,10 @@ const jobControllers = require("../controllers/jobControllers");
 
 // api route to push straight to mongoDB
 jobControllers.getQSJobs();
-jobControllers.getCMJobs();
-jobControllers.getCAJobs();
 jobControllers.getElecJobs();
 jobControllers.getPlumberJobs();
-jobControllers.getAirconJobs();
 jobControllers.getBricklayerJobs();
 jobControllers.getCarpenterJobs();
-jobControllers.getPlastererJobs();
 
 // front-end job routes
 router.get("/jobs", jobControllers.getJobs);
